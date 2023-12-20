7962a0d9cbc5d6bb1d19a679958657075a028747
Alexander von Studnitz <studnitz@hey.com>
2023-12-20 11:34:53 +0100

    Upgrade date-fns and eslint

--- src/date.test.ts
+++ src/date.test.ts
@@ -1,51 +1,51 @@
-import { expect, it } from 'vitest'
+import { toDate } from "date-fns";
+import { expect, it } from "vitest";
+
 import {
-  toDate,
   toGermanDateString,
   toGermanTimeString,
-  toGermanString,
-  toUtcDateString,
-  toUtcTimeString,
-  toUtcString,
-} from './date'
+  toGermanDateTimeString,
+} from "./date";
 
-it('toDate', () => {
-  const fromIsoString = toDate('2020-01-01T00:00:00.000Z')
-  const fromUnixTimestamp = toDate(1_577_836_800_000)
-  expect(toDate(new Date())).toBeInstanceOf(Date)
-  expect(fromIsoString).toBeInstanceOf(Date)
-  expect(fromIsoString.getTime()).toBe(1_577_836_800_000)
-  expect(fromUnixTimestamp).toBeInstanceOf(Date)
-  expect(fromUnixTimestamp.getTime()).toBe(1_577_836_800_000)
-})
+it("toDate", () => {
+  const fromIsoString = toDate("2020-01-01T00:00:00.000Z");
+  const fromUnixTimestamp = toDate(1_577_836_800_000);
+  expect(toDate(new Date())).toBeInstanceOf(Date);
+  expect(fromIsoString).toBeInstanceOf(Date);
+  expect(fromIsoString.getTime()).toBe(1_577_836_800_000);
+  expect(fromUnixTimestamp).toBeInstanceOf(Date);
+  expect(fromUnixTimestamp.getTime()).toBe(1_577_836_800_000);
+});
 
-it('toGermanDateString with default timezone', () => {
-  const date = toDate('2020-01-31T00:00:00.000Z')
-  expect(toGermanDateString(date)).toBe('31.01.2020')
-})
+it("toGermanDateString with default timezone", () => {
+  const date = toDate("2020-01-31T00:00:00.000Z");
+  expect(toGermanDateString(date)).toBe("31.01.2020");
+});
 
-it('toGermanDateString with UTC Timezone', () => {
-  const date = toDate('2020-01-31T00:00:00.000Z')
-  expect(toGermanDateString(date, { timeZone: 'UTC' })).toBe('31.01.2020')
-})
+it("toGermanDateString with UTC Timezone", () => {
+  const date = toDate("2020-01-31T00:00:00.000Z");
+  expect(toGermanDateString(date, { timeZone: "UTC" })).toBe("31.01.2020");
+});
 
-it('toGermanTimeString with default timezone', () => {
+it("toGermanTimeString with default timezone", () => {
   // this is 01:00 because of the timezone offset
-  const date = toDate('2020-01-01T00:00:00.000Z')
-  expect(toGermanTimeString(date)).toBe('01:00')
-})
+  const date = toDate("2020-01-01T00:00:00.000Z");
+  expect(toGermanTimeString(date)).toBe("01:00");
+});
 
-it('toGermanTimeString with UTC timezone', () => {
-  const date = toDate('2020-01-01T00:00:00.000Z')
-  expect(toGermanTimeString(date, { timeZone: 'UTC' })).toBe('00:00')
-})
+it("toGermanTimeString with UTC timezone", () => {
+  const date = toDate("2020-01-01T00:00:00.000Z");
+  expect(toGermanTimeString(date, { timeZone: "UTC" })).toBe("00:00");
+});
 
-it('toGermanString with default timezone', () => {
-  const date = toDate('2020-01-31T00:00:00.000Z')
-  expect(toGermanString(date)).toBe('31.01.2020, 01:00')
-})
+it("toGermanDateTimeString with default timezone", () => {
+  const date = toDate("2020-01-31T00:00:00.000Z");
+  expect(toGermanDateTimeString(date)).toBe("31.01.2020, 01:00");
+});
 
-it('toGermanString with UTC timezone', () => {
-  const date = toDate('2020-01-31T00:00:00.000Z')
-  expect(toGermanString(date, { timeZone: 'UTC' })).toBe('31.01.2020, 00:00')
-})
+it("toGermanDateTimeString with UTC timezone", () => {
+  const date = toDate("2020-01-31T00:00:00.000Z");
+  expect(toGermanDateTimeString(date, { timeZone: "UTC" })).toBe(
+    "31.01.2020, 00:00"
+  );
+});
--- src/date.ts
+++ src/date.ts
@@ -1,74 +1,77 @@
-import { startOfToday } from 'date-fns'
-import type { Dateable } from './types'
+import { startOfToday, toDate } from "date-fns";
 
-/**
- * Parse a date, an unix timestamp or a date string into a Date object.
- *
- * @example ```ts
- * const fromIsoString = toDate('2020-01-01T00:00:00.000Z')
- * const fromTimestamp = toDate(1577836800000)
- * const fromDateObject = toDate(new Date())
- * ```
- */
-export function toDate(date: Dateable): Date {
-  if (date instanceof Date) {
-    // return date clone
-    return new Date(date.getTime())
-  }
-
-  return new Date(date)
-}
+import type { Dateable } from "./types";
 
 /**
  * Return a german date string, e.g. "31.12.2019". Defaults to Europe/Berlin timezone.
  */
-export function toGermanDateString(date: Dateable, options?: Intl.DateTimeFormatOptions): string {
-  return toDate(date).toLocaleDateString('de-DE', {
-    day: '2-digit',
-    month: '2-digit',
-    year: 'numeric',
-    timeZone: 'Europe/Berlin',
+export function toGermanDateString(
+  date: Dateable,
+  options?: Intl.DateTimeFormatOptions
+): string {
+  return toDate(date).toLocaleDateString("de-DE", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    timeZone: "Europe/Berlin",
     ...options,
-  })
+  });
 }
 
 /**
  * Return a german time string, e.g. "18:00". Defaults to Europe/Berlin timezone.
  */
-export function toGermanTimeString(date: Dateable, options?: Intl.DateTimeFormatOptions): string {
-  return toDate(date).toLocaleTimeString('de-DE', {
-    hour: '2-digit',
-    minute: '2-digit',
-    timeZone: 'Europe/Berlin',
+export function toGermanTimeString(
+  date: Dateable,
+  options?: Intl.DateTimeFormatOptions
+): string {
+  return toDate(date).toLocaleTimeString("de-DE", {
+    hour: "2-digit",
+    minute: "2-digit",
+    timeZone: "Europe/Berlin",
     ...options,
-  })
+  });
 }
 
 /**
  * Return a german date and time string, e.g. "31.12.2019, 18:00". Defaults to Europe/Berlin timezone.
  */
-export function toGermanString(date: Dateable, options?: Intl.DateTimeFormatOptions): string {
-  return toDate(date).toLocaleString('de-DE', {
-    day: '2-digit',
-    month: '2-digit',
-    year: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-    timeZone: 'Europe/Berlin',
+export function toGermanDateTimeString(
+  date: Dateable,
+  options?: Intl.DateTimeFormatOptions
+): string {
+  return toDate(date).toLocaleString("de-DE", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    timeZone: "Europe/Berlin",
     ...options,
-  })
+  });
+}
+
+/**
+ * Return a german date and time string, e.g. "31.12.2019, 18:00". Defaults to Europe/Berlin timezone.
+ * @deprecated Use `toGermanDateTimeString` instead.
+ */
+export function toGermanString(
+  date: Dateable,
+  options?: Intl.DateTimeFormatOptions
+): string {
+  return toGermanDateTimeString(date, options);
 }
 
 /**
  * Return the current time as a Date object.
  */
 export function now(): Date {
-  return new Date()
+  return new Date();
 }
 
 /**
  * Return the start of today as a Date object.
  */
 export function today(): Date {
-  return startOfToday()
+  return startOfToday();
 }
